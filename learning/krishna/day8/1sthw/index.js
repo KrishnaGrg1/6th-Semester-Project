dac4340db8e47e55e39d5b36862047dab4d526f6
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2024-12-31 18:23:50 +0545

    adding day8 homework 1st

--- learning/krishna/day8/1sthw/index.js
+++ learning/krishna/day8/1sthw/index.js
@@ -1,2 +1,46 @@
 //Alt+shift+f to 
+ //1. Change the Request methods from GET to :
+//  - POST for those routes in which new data is added
+//  - PUT for those in which data is edited 
+//  - DELETE for those in which existing data is delted
+import express from "express";
+import { viewall,addtodo, editTodo,deleteTodo} from "./routes/view.js";
+const port=9000;
+const server=express();
+server.use(express.json())
 
+
+server.get('/view',(req,res)=>{
+    res.send(viewall());
+})
+
+server.post('/view/task/:task',(req,res)=>{
+    try {
+        return res.json(addtodo(req.params.task));
+    } catch (e) {
+        return res.status(400).json({error: e.message})
+    }
+})
+
+server.put('/view/task/:taska/:taskb',(req,res)=>{
+    try {
+        return res.json( editTodo(req.params.taska,req.params.taskb));
+    } catch (e) {
+        return res.status(400).json({error: e.message})
+    }
+})
+
+server.delete('/view/task/:task',(req,res)=>{
+    try {
+        return res.json( deleteTodo(req.params.task));
+    } catch (e) {
+        return res.status(400).json({error: e.message})
+    }
+      
+})
+
+
+
+server.listen(port, function () {
+    console.log("Server running on port", port)
+})
---
+++ learning/krishna/day8/1sthw/routes/view.js
@@ -0,0 +1,69 @@
+import { readFileSync, writeFileSync} from "fs";
+
+
+function viewall(){
+   const view=readFileSync('/Users/krishnabahadurgurung/Documents/Project/sample4/6th-Semester-Project/learning/krishna/day8/1sthw/view.json',{
+    encoding:"utf-8" })
+    const parsedData=JSON.parse(view);
+    return parsedData;
+}
+
+function addtodo(taskname){
+
+    let {count,todo}=viewall();
+
+   if(todo.includes(taskname)){
+    throw new Error (`${taskname} is already exist`);
+   }else{
+    todo=[...todo,taskname];
+    let write={todo,count};
+    let obj=JSON.stringify(write);
+    writeFileSync('/Users/krishnabahadurgurung/Documents/Project/sample4/6th-Semester-Project/learning/krishna/day8/1sthw/view.json',obj,{
+        encoding:'utf8'
+    })
+    return write;
+   }
+
+}
+
+function editTodo(taska,taskb){
+    let {count,todo}=viewall();
+
+    if(todo.includes(taska)){
+        if(todo.includes(taskb)){
+            throw new Error (`${taskb} is already exist`);
+        }else{
+            const index = todo.indexOf(taska);
+            todo[index]=taskb;
+            let write={todo,count};
+            let obj=JSON.stringify(write);
+            writeFileSync('/Users/krishnabahadurgurung/Documents/Project/sample4/6th-Semester-Project/learning/krishna/day8/1sthw/view.json',obj,{
+                encoding:'utf8'
+            })
+            return write;
+        }
+       }else{
+        throw new Error (`${taska} not found`);
+      
+    }
+}
+
+
+function deleteTodo(taskname){
+    const {todo,count}=viewall();
+    if(todo.includes(taskname)){
+        const todoIndex=todo.indexOf(taskname)
+        todo.splice(todoIndex,1);
+        const write={todo,count};
+        let obj=JSON.stringify(write);
+        writeFileSync('/Users/krishnabahadurgurung/Documents/Project/sample4/6th-Semester-Project/learning/krishna/day8/1sthw/view.json',obj,{
+            encoding:'utf8'
+        })
+        return write;
+    }else{
+        throw new Error (`${taska} not found`);
+    }
+}
+
+
+export {viewall,addtodo, editTodo,deleteTodo}
