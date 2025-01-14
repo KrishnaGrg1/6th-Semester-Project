bc9558dfeb6ab18d4399cb020aa37b2e95e46afe
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2025-01-14 23:49:42 +0545

    managed code

--- code/sample2/routes/routes.js
+++ code/sample2/routes/routes.js
@@ -1,5 +1,5 @@
 // routes/routes.js
-const express = require('express');
+import express from 'express';
 const UserModel=require('../models/model');
 const router = express.Router();
 const { register, login } = require('../controller/url');
